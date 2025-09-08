6a0e0d9df70df6e1accebc13b7379a45fe35871f
charlotteedwardson <edwardchar2@myvuw.ac.nz>
2025-09-08 21:25:38 +1200

    stars, sparkles, clouds + map()
    added map() function to background rgb. added more visuals to screen (stars, sparkles, stars)
    
    Sparkles follow mouse

--- music_starter.js
+++ music_starter.js
@@ -1,18 +1,59 @@
-let sunX = -40; // starts just offscreen
+let cloudY = -100; // starts just offscreen
+let cloudSize = 175;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(140,160,230)
 
+  var r = 120;
+  var g = 100;
+  var b = 250;
+
+  r = map(frameCount-500, 0, 960, 190, 90); // -500 frame count so background changes slower
+  g = map(frameCount-500, 0, 960, 200, 100);
+  b = map(frameCount-500, 0, 960, 250, 150);
+  background(r, g, b);
+  
+
+
+
+  //draw stars
+   push();
+  scale(0.9)
+  translate(225, 100)
+  drawStar()
+  pop();
+
+  push();
+  scale(0.7)
+  translate(100,-50)
+  drawStar()
+  pop();
+
+  push();
+  scale(0.6)
+  translate(-100, 350);
+  drawStar()
+  pop();
+
+  //draw sparkles in
+  drawSparkle()
+
+  //draw clouds
+  push();
+  translate(100, -150)
+  drawCloud() 
+  pop();
+
+  push();
+  scale(0.8)
+  translate(150, -400);
   drawCloud()
+  pop();
 
-   fill(225, 90, 190);
-   ellipse(sunX-20, 500, 50)
-sunX = sunX +0.7
-
-if(sunX > 570){ // ends just off screen
-sunX = -10
-
-}
+  push();
+  scale(0.8)
+  translate(-200, -250);
+  drawCloud()
+  pop();
 }
 
 function drawCloud() {
@@ -23,30 +64,84 @@ var ColourCloudShadow = color(200, 160, 210) //200, 160, 210
 
 noStroke()
 fill(ColourCloudLight) //highlight
-ellipse(250, 150, 200)
+ellipse(250, cloudY+150, cloudSize)
 
 noStroke()
 fill(ColourCloudLight) //highlight
-ellipse(350, 195, 160, 130)
+ellipse(350, cloudY+195, cloudSize-40, cloudSize-70)
 
 noStroke()
 fill(ColourCloudShadow)
-ellipse(170, 185, 160)
+ellipse(170, cloudY+185, cloudSize-40)
 
 noStroke()
 fill(ColourCloudShadow)
-ellipse(240, 170, 200)
+ellipse(240, cloudY+170, cloudSize)
 
 noStroke()
 fill(ColourCloudLight) //highlight
-ellipse(280, 190, 200, 150)
+ellipse(280, cloudY+190, cloudSize, cloudSize-50)
 
 noStroke()
 fill(ColourCloudShadow)
-ellipse(240, 200, 250, 140)
+ellipse(240, cloudY+200, cloudSize+50, cloudSize-60)
 
+cloudY = cloudY +0.25
 
+if(cloudY > 1600){ // ends just off screen
+cloudY = -100
+}
 
+}
 
+function drawSparkle() {
 
+var sparkle = {
+   locationX: random(width),
+   locationY: random(height),
+   size: random(1, 6)
+}
+
+fill(255); //white sparkles
+noStroke();
+ellipse(mouseX, mouseY, sparkle.size, sparkle.size);
+ellipse(sparkle.locationX, sparkle.locationY, sparkle.size, sparkle.size);
+
+}
+
+function drawStar() {
+let opacityStar = color(250, 240, 255);
+   beginShape();
+   fill(250, 240, 255);
+  vertex(250, 150); 
+  vertex(300, 200);
+  vertex(250, 250);
+  vertex(200, 200); 
+  endShape(CLOSE); // base diamond shape done
+
+   beginShape();
+   fill(250, 240, 255); 
+   vertex(250, 100);
+   vertex(290, 200);
+   vertex(250, 300);
+   vertex(210, 200); 
+   endShape(CLOSE);
+
+   beginShape();
+   opacityStar.setAlpha(120);
+   fill(opacityStar); 
+   vertex(250, 75);
+   vertex(315, 200);
+   vertex(250, 325);
+   vertex(185, 200); 
+   endShape(CLOSE);
+
+   beginShape();
+   opacityStar.setAlpha(120);
+   fill(opacityStar); 
+   vertex(250, 50);
+   vertex(340, 200);
+   vertex(250, 350);
+   vertex(160, 200); 
+   endShape(CLOSE);
 }
